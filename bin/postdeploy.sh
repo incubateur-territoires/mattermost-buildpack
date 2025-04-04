4b86a31b97d84807deed6d275110da10c91f6969
Oscar Hemelaar <oscar.hemelaar@qonfucius.team>
2025-04-04 17:26:28 +0200

    WIP

--- bin/postdeploy.sh
+++ bin/postdeploy.sh
@@ -3,6 +3,8 @@
 
 export MM_SERVICESETTINGS_LISTENADDRESS=":${PORT}"
 
+echo "postdeploy: Starting"
+
 plugins_list=$(find /app/mattermost/postdeploy_plugins/. -maxdepth 1 -name '*.tar.gz' | tr '\n' ',')
 
 for plugin in $(echo "$plugins_list" | tr ',' '\n')
