5bc60875280628edf4c2762a1b2b77ba6e646441
Oscar Hemelaar <oscar.hemelaar@qonfucius.team>
2025-04-04 17:57:59 +0200

    WIP

--- bin/postdeploy.sh
+++ bin/postdeploy.sh
@@ -6,9 +6,10 @@ export MM_SERVICESETTINGS_LISTENADDRESS=":${PORT}"
 echo "postdeploy: Starting"
 
 plugins_list=$(find /app/mattermost/postdeploy_plugins/. -maxdepth 1 -name '*.tar.gz' | tr '\n' ',')
+/app/mattermost/bin/mmctl auth login $MATTERMOST_URL --access-token "MATTERMOST_ADMIN_TOKEN"
 
 for plugin in $(echo "$plugins_list" | tr ',' '\n')
 do
   echo "postdeploy: Installing plugin $plugin"
-  tar xzf $plugin --directory /app/mattermost/plugins
+  /app/mattermost/bin/mmctl plugin add "$plugin"
 done
