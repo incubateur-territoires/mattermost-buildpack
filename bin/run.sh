64f58aef57b751539eb340e7432ed0a915f4cc68
Oscar Hemelaar <oscar.hemelaar@qonfucius.team>
2025-04-07 17:04:52 +0200

    refactor: move mandatory env var to entrypoint

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # usage: /app/mattermost/bin/run
 
+export MM_PLUGINSETTINGS_CLIENTDIRECTORY="/app/mattermost/client/plugins"
 export MM_SERVICESETTINGS_LISTENADDRESS=":${PORT}"
 
 /app/mattermost/bin/mattermost
